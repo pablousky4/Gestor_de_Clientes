e5b6eead71c6da53ac7c3f082d651a344fe8f743
Pablo Castro Rodriguez <69274746+pablousky4@users.noreply.github.com>
2025-04-08 16:56:05 +0000

    tabuleo

--- gestor/database.py
+++ gestor/database.py
@@ -1,34 +1,34 @@
 class Cliente:
     def __init__(self, dni, nombre, apellido):
-    self.dni = dni
-    self.nombre = nombre
-    self.apellido = apellido
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
     def __str__(self):
-    return f"({self.dni}) {self.nombre} {self.apellido}"
+        return f"({self.dni}) {self.nombre} {self.apellido}"
 
 class Clientes:
     # Lista de clientes
     lista = []
     @staticmethod
     def buscar(dni):
-    for cliente in Clientes.lista:
-    if cliente.dni == dni:
-    return cliente
+        for cliente in Clientes.lista:
+            if cliente.dni == dni:
+                return cliente
     @staticmethod
     def crear(dni, nombre, apellido):
-    cliente = Cliente(dni, nombre, apellido)
-    Clientes.lista.append(cliente)
-    return cliente
+        cliente = Cliente(dni, nombre, apellido)
+        Clientes.lista.append(cliente)
+        return cliente
     @staticmethod
     def modificar(dni, nombre, apellido):
-    for i, cliente in enumerate(Clientes.lista):
-    if cliente.dni == dni:
-    Clientes.lista[i].nombre = nombre
-    Clientes.lista[i].apellido = apellido
-    return Clientes.lista[i]
+        for i, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                Clientes.lista[i].nombre = nombre
+                Clientes.lista[i].apellido = apellido
+                return Clientes.lista[i]
     @staticmethod
     def borrar(dni):
-    for i, cliente in enumerate(Clientes.lista):
-    if cliente.dni == dni:
-    cliente = Clientes.lista.pop(i)
-    return cliente
+        for i, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                cliente = Clientes.lista.pop(i)
+                return cliente
